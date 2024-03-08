127a342abfc8f49e9c5ee720c97ce01cd1c11a0d
sooperdoob <nuclearpit@gmail.com>
2024-03-07 22:29:40 -0500

    doobers ocd and fixed missing method in exports

--- models/userModel.js
+++ models/userModel.js
@@ -1,20 +1,21 @@
 const db = require('../util/db');
 const prisma = db.prisma;
 const getUserByUsername = async (username) => {
-  return prisma.user.findUnique({where: {username: username}});
+  return prisma.user.findUnique({ where: { username: username }});
 };
 
 const getUserById = async (id) => {
-  return prisma.user.findUnique({where: { id : id }});
+  return prisma.user.findUnique({ where: { id : id }});
 };
 
 const createUser = async ({ username, hashedPw }) => {
   const discordId = "";
   const mcUuid = "";
-  await prisma.user.create({data: {username,hashedPw,discordId,mcUuid}});
+  await prisma.user.create({ data: { username, hashedPw, discordId, mcUuid }});
 };
 
 module.exports = {
   getUserByUsername,
   createUser,
+  getUserById,
 };
