e0b23a647fc56818269a4b6a88ed9be739912ebf
sooperdoob <nuclearpit@gmail.com>
2024-03-08 16:28:48 -0500

    ocd

--- models/userModel.js
+++ models/userModel.js
@@ -13,15 +13,16 @@ const getUserById = async (id) => {
 
 const getUserByDiscordId = async (discordId) => {
   await prisma.user.findUnique({ where: { discordId: discordId }});
-}
+};
 
 const createUser = async ({ username, hashedPw }) => {
   await prisma.user.create({ data: { username, hashedPw }});
 };
+
 const createUserDiscord = async ({ discordId, username, avatar, email }) => {
   const user = await prisma.user.create({ data: { discordId, username }});
   await prisma.profile.create({ data: { userId: user.id, avatar: avatar, email: email, username: username }});
-}
+};
 
 module.exports = {
   getUserByUsername,
