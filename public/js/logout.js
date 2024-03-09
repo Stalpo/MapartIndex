b2ecfb6ae1cf7e8915e2ca066ddf9bc35a7e7a9f
sooperdoob <nuclearpit@gmail.com>
2024-03-08 22:08:08 -0500

    fixed logout cookie bug

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,13 +1,13 @@
 let countdown = 5;
 
 const updateCountdown = () => {
+  document.cookie = 'token=; path=/;';
   document.getElementById('countdown').textContent = countdown;
   countdown--;
 
   if (countdown >= 0) {
     setTimeout(updateCountdown, 1000);
   } else {
-    document.cookie = 'token=; path=/;';
     window.location.href = '/';
   }
 };
