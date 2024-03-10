044eb883872f8817f58ae86391b8de959c6d7bb5
sooperdoob <nuclearpit@gmail.com>
2024-03-10 03:50:12 -0400

    fixed bug on profile page if logged out

--- index.js
+++ index.js
@@ -85,9 +85,11 @@ app.get('/logout', (req, res) => {
 // Profile route
 app.get('/profile', async (req, res) => {
   const userId = res.locals.userId;
-  res.locals.profile = await profileController.getProfileById(userId);
-  res.locals.userMaps = await profileController.getAllMapsForUserId(userId);
-
+  if (userId) {
+    res.locals.profile = await profileController.getProfileById(userId);
+    res.locals.userMaps = await profileController.getAllMapsForUserId(userId);
+  }
+  
   // Debugging the profile joined date not being correct.
   // console.log(res.locals.profile);
 
