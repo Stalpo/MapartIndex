a6d1e8d191a9da17483d816e75309160ae86ed34
THCFree <me@thcfree.dev>
2024-03-11 17:09:10 +0100

    Added user id / profile endpoint

--- routes/api/user.js
+++ routes/api/user.js
@@ -29,4 +29,32 @@ router.get('/maps', async (req, res) => {
   res.status(404).json({ error: 'Map ids not found' });
 });
 
+/**
+ * @swagger
+ * /api/user/{id}:
+ *  get:
+ *    description: Returns a user profile defined by the id provided.
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        type: string
+ *        format: uuid
+ *        required: true
+ *        description: The user id to search for
+ *    responses:
+ *      200:
+ *        description: Returns a user profile defined by the id provided.
+ *      404:
+ *        description: User id not found.
+ *    tags:
+ *      - User
+ */
+router.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    const result = await profileController.getProfileById(id);
+    if (result) return res.status(200).json(result);
+    res.status(404).json({ error: "User id not found" });
+
+});
+
 module.exports = router;
