23825d38a7ae01d0c9406e9515b314b861146714
sooperdoob <nuclearpit@gmail.com>
2024-03-16 08:07:48 -0400

    code-stash not complete but working joinImages

--- public/js/joinImages.js
+++ public/js/joinImages.js
@@ -7,18 +7,24 @@ const joinImages = (width, height, imgurls) => {
 
   canvas.width = 128 * width;
   canvas.height = 128 * height;
-  
-  Promise.all(imgurls.map(downloadAsset)).then(() => {
-    for(let i = 0; i < imgs.length; i++){
-      ctx.drawImage(imgs[i], (i % width) * 128, Math.floor(i / width) * 128);
-    }
 
-    const url = canvas.toDataURL();
+  return new Promise((resolve, reject) => {
+    Promise.all(imgurls.map(downloadAsset))
+      .then(() => {
+        for (let i = 0; i < imgs.length; i++) {
+          ctx.drawImage(imgs[i], (i % width) * 128, Math.floor(i / width) * 128);
+        }
 
-    // data url to new image
-    return url;
+        const url = canvas.toDataURL();
+        resolve(url); // Resolve with the data URL of the merged image
+      })
+      .catch(error => {
+        console.error('Error merging images:', error);
+        reject(error); // Reject with the error message
+      });
   });
 };
+
 
 // promise to load needed image
 function downloadAsset(assetName) {
